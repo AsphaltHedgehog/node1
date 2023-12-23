cea2a27057bad1552427621391f47b8c33c90641
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2023-12-23 17:11:59 +0200

    complete tasks

--- contacts.js
+++ contacts.js
@@ -1,37 +1,63 @@
-const fs = require('fs').promises;
+const fs = require('fs/promises');
 const path = require('path');
-
-
-
+const crypto = require('crypto');
 
 const contactsPath = path.join(__dirname, 'db', 'contacts.json');
 
-
-// TODO: задокументувати кожну функцію
-function listContacts() {
-  // ...твій код. Повертає масив контактів.
+async function listContacts() {
   return fs.readFile(contactsPath).then(data => {
     const array = JSON.parse(data);
-    console.log(array)
+    return array;
   }).catch(err => console.error(err.msg));
 }
 
-function getContactById(contactId) {
-  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
-  return fs.readFile(contactsPath).then(data => {
-    const array = JSON.parse(data);
-    if (array.includes(contactId)) {
-      return array.find(contact => contact.id === contactId);
-    } else {
-      return null;
+async function getContactById(contactId) {
+  return fs.readFile(contactsPath).then( async data => {
+    const array = await JSON.parse(data);
+    return array.find(contact => contact.id === contactId) || null;
+    
+  }).catch(err => console.error(err.msg));
+}
+
+async function removeContact(contactId) {
+  const array = JSON.parse(await fs.readFile(contactsPath));
+
+  const indexOfContact = array.findIndex(contact => contact.id === contactId);
+
+  if (indexOfContact === -1) {
+    return null;
+  } else {
+    const deletedContact = array.splice(indexOfContact, 1);
+    const newArray = array.filter(contact => contact.id !== contactId);
+    fs.writeFile(contactsPath, JSON.stringify(newArray));
+    return deletedContact;
+  }
+
+}
+
+async function addContact(name, email, phone) {
+  const array = JSON.parse(await fs.readFile(contactsPath));
+  let contact = {};
+
+  if (name === '' || email === '' || phone === '') {
+    return null;
+  } else {
+    contact = {
+      id: crypto.randomUUID().toString(),
+      name,
+      email,
+      phone
     }
-  }).catch(err => console.error(err.msg));
+  };
+  const newArray = array.concat(contact);
+  fs.writeFile(contactsPath, JSON.stringify(newArray, null, 2));
+
+  return contact;
 }
 
-function removeContact(contactId) {
-  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
-}
-
-function addContact(name, email, phone) {
-  // ...твій код. Повертає об'єкт доданого контакту. 
-}
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact
+};
